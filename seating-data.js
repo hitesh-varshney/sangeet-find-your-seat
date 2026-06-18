9b10204129c90f70a0293dad3d955950a12dfc88
Hitesh Varshney <vsuperhit@gmail.com>
2026-06-17 23:18:51 -0500

    Add files via upload

--- seating-data.js
+++ seating-data.js
@@ -1,176 +1,171 @@
 window.SEATING_DATA = {
- "updated": "(draft)",
+ "updated": "June 17, 2026",
  "tables": {
   "head": {
-   "label": "Head Table",
+   "label": "Sweetheart Table",
    "guests": [
-    "Hitesh Varshney",
-    "Tiffany Louie"
+    "Tiffany Louie",
+    "Hitesh Varshney"
    ]
   },
   "T1": {
-   "label": "1",
+   "label": "10",
    "guests": [
-    "Elaine Dea",
-    "Michelle Gresko",
-    "Ilesh Singh",
-    "Nikki Varshney",
-    "Anisha Rao",
-    "Harish Kesavan",
-    "Kesavan Velu",
-    "Parvathi Kesavan",
-    "Vinodhini Kesavan"
+    "Blake Rankin",
+    "Megan Cicotte",
+    "Justin Seeke",
+    "Renee Seeke",
+    "John Jackson",
+    "Zaya Amgaabaatar",
+    "Andrew Karaki",
+    "Ari Alexanian",
+    "Emily Dixon",
+    "Rahul Gangwani"
    ]
   },
   "T2": {
-   "label": "2",
-   "guests": [
-    "Lisa Yuk Louie",
-    "Matt Kazmierski",
-    "Michael Page",
-    "Monika Garg",
-    "Saloni Garg",
-    "Sanjay Garg",
-    "Snehal Garg",
-    "Nivedita Ahuja",
-    "Sanjeev Ahuja"
-   ]
-  },
-  "T3": {
-   "label": "3",
-   "guests": [
-    "Ari Alexanian",
-    "Jireh Louie",
-    "Kalle Solberg",
-    "Shivansh Ahuja",
-    "Garrett Smiley",
-    "Ritu Agrawal",
-    "Vadini Agrawal",
-    "Vinay Agrawal",
-    "Vishal Agrawal"
-   ]
-  },
-  "T4": {
-   "label": "4",
-   "guests": [
-    "Emily Dixon",
-    "Rahul Gangwani",
-    "Dolly Pal",
-    "Murtaza Jamal",
-    "Ravinder Pal",
-    "Shazia Jamal",
-    "Veena Pandey",
-    "Kaushal Kishore",
-    "Rekha Kishore"
-   ]
-  },
-  "T5": {
-   "label": "5",
-   "guests": [
-    "Andrew Karaki",
-    "David Kaftan",
-    "Isha Varshney",
-    "Brandon Lau",
-    "Darren Lau",
-    "Jane Szymanski",
-    "Linda Lau",
-    "Matt McEwen",
-    "Samantha Schad"
-   ]
-  },
-  "T6": {
-   "label": "6",
+   "label": "11",
    "guests": [
     "Claudia Ellebrecht",
     "Steve Sismelich",
-    "Alka Gupta",
-    "Pankaj Gupta",
+    "Megan Barker",
+    "Zach Lugo",
+    "Vinodhini Kesavan",
+    "Matt McEwen",
+    "Samantha Schad",
+    "Matt Kazmierski",
+    "Michelle Gresko",
+    "Zak Kolar"
+   ]
+  },
+  "T3": {
+   "label": "1",
+   "guests": [
+    "Hemant Varshney",
+    "Seema Varshney",
+    "Mukesh Singh",
+    "Sarala Pandey",
+    "Ilesh Singh",
+    "Nikki Varshney",
+    "Sushil Varshney",
+    "Vijay Laxmi Gupta"
+   ]
+  },
+  "T4": {
+   "label": "2",
+   "guests": [
+    "Joseph Louie",
+    "Mary Dea",
     "Candice Wong",
     "Richard Wong",
     "Vicky Wong",
     "Zoey Wong",
-    "Rohit Murjani"
+    "Pauline Chang",
+    "Philip Dea",
+    "Elaine Dea",
+    "Lisa Yuk Louie"
    ]
   },
-  "T7": {
-   "label": "7",
+  "T5": {
+   "label": "3",
    "guests": [
-    "Anil Murjani",
     "James Murjani",
-    "Kavita Murjani",
     "Monisha Murjani",
-    "Girija Trivedi",
-    "Rahul Trivedi",
-    "John Jackson",
-    "Zaya Amgaabaatar",
+    "Shirome Mehta",
+    "Rohit Murjani",
+    "Michael Page",
+    "Snehal Garg",
+    "Kalle Solberg",
+    "Shivansh Ahuja",
     "Shivika Ahuja"
    ]
   },
-  "T8": {
-   "label": "8",
+  "T6": {
+   "label": "4",
    "guests": [
-    "Deepak Mehta",
-    "Megan Mehta",
-    "Monica Mehta",
-    "Shirome Mehta",
-    "Megan Barker",
-    "Zach Lugo",
-    "Ratish Varshney",
-    "Sadhana Varshney",
-    "Sushil Varshney"
+    "Jireh Louie",
+    "Brandon Lau",
+    "Darren Lau",
+    "Jane Szymanski",
+    "Linda Lau",
+    "Chris Hui",
+    "Naomi Louie",
+    "Mitchell Yau",
+    "Ryan Yau"
    ]
   },
-  "T9": {
-   "label": "9",
+  "T7": {
+   "label": "5",
    "guests": [
     "Ashish Varshney",
     "Gia Varshney",
     "Meenal Varshney",
     "Tanmay Varshney",
-    "Blake Rankin",
-    "Megan Cicotte",
-    "Deeksha Varshney",
-    "Sumit Varshney",
-    "Vijay Laxmi Gupta"
+    "David Kaftan",
+    "Isha Varshney",
+    "Kaushal Kishore",
+    "Rekha Kishore",
+    "Claire Mercer",
+    "Rahul Varshney"
+   ]
+  },
+  "T8": {
+   "label": "6",
+   "guests": [
+    "Anil Murjani",
+    "Kavita Murjani",
+    "Deepak Mehta",
+    "Monica Mehta",
+    "Monika Garg",
+    "Sanjay Garg",
+    "Ritu Agrawal",
+    "Vishal Agrawal",
+    "Nivedita Ahuja",
+    "Sanjeev Ahuja"
+   ]
+  },
+  "T9": {
+   "label": "7",
+   "guests": [
+    "Megan Mehta",
+    "Jeremy Louie",
+    "Krissa Huang",
+    "Anisha Rao",
+    "Harish Kesavan",
+    "Saloni Garg",
+    "Varun Agarwal",
+    "Garrett Smiley",
+    "Vadini Agrawal",
+    "Vinay Agrawal"
    ]
   },
   "T10": {
-   "label": "10",
-   "guests": [
-    "Zak Kolar",
-    "Claire Mercer",
-    "Rahul Varshney",
-    "Sonia Agarwal",
-    "Sunil Agarwal",
-    "Varun Agarwal",
-    "Chris Hui",
-    "Naomi Louie"
-   ]
-  },
-  "T11": {
-   "label": "11",
-   "guests": [
-    "Joseph Louie",
-    "Mary Dea",
-    "Justin Seeke",
-    "Renee Seeke",
-    "Mukesh Singh",
-    "Sarala Pandey",
-    "Mitchell Yau",
-    "Ryan Yau"
-   ]
-  },
-  "T12": {
-   "label": "12",
+   "label": "8",
    "guests": [
     "Anand Balsekar",
     "Leena Balsekar",
-    "Hemant Varshney",
-    "Seema Varshney",
-    "Jeremy Louie",
-    "Krissa Huang",
-    "Pauline Chang",
-    "Philip Dea"
+    "Alka Gupta",
+    "Pankaj Gupta",
+    "Kesavan Velu",
+    "Parvathi Kesavan",
+    "Girija Trivedi",
+    "Rahul Trivedi",
+    "Sonia Agarwal",
+    "Sunil Agarwal"
+   ]
+  },
+  "T11": {
+   "label": "9",
+   "guests": [
+    "Ratish Varshney",
+    "Sadhana Varshney",
+    "Deeksha Varshney",
+    "Sumit Varshney",
+    "Dolly Pal",
+    "Murtaza Jamal",
+    "Ravinder Pal",
+    "Shazia Jamal",
+    "Veena Pandey"
    ]
   }
  }
